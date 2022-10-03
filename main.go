0277ee707b99913f092c12d5677ad3dccf8b071b
Mauro Alderete <alderete.mauro@gmail.com>
2022-10-03 16:23:56 -0300

    feat: sample of use the wallet to query an address

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	_ "embed"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/mauroalderete/weasel/wallet"
 )
 
 //go:generate build/get_version.sh
@@ -13,5 +17,39 @@ import (
 var version string
 
 func main() {
-	fmt.Println("Hi world!")
+
+	const server = "https://cloudflare-eth.com"
+
+	fmt.Printf("Connecting... ")
+	client, err := ethclient.Dial(server)
+	if err != nil {
+		fmt.Printf("[FAIL]\n")
+		log.Fatalf("failed to connect to ethclient: %v", err)
+	}
+
+	_ = client
+	fmt.Printf("[OK]\n")
+
+	fmt.Printf("Opening wallet... ")
+	w, err := wallet.New("0x71c7656ec7ab88b098defb751b7401b5f6d8976f", client)
+	if err != nil {
+		fmt.Printf("[FAIL]\n")
+		log.Fatalf("failed to instance a new wallet: %v", err)
+	}
+
+	err = w.Update()
+	if err != nil {
+		fmt.Printf("[FAIL]\n")
+		log.Fatalf("failed to open the wallet with address %s: %v", w.Address().Hex(), err)
+	}
+
+	fmt.Printf("[OK]\n\n")
+
+	fmt.Printf("Address: %s\n", w.Address().Hex())
+	fmt.Printf("Balance: %sWEI\n", w.Balance().Wei().String())
+	fmt.Printf("Balance: %sETH\n", w.Balance().Eth().String())
+	fmt.Printf("Pending balance: %sWEI\n", w.PendingBalance().Wei().String())
+	fmt.Printf("Pending balance: %sETH\n", w.PendingBalance().Eth().String())
+
+	fmt.Printf("Bye\n")
 }
