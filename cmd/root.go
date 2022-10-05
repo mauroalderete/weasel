17a12951ed33227651a123b4eaeb6311eea85615
Mauro Alderete <alderete.mauro@gmail.com>
2022-10-05 16:24:57 -0300

    feat: implement cobra commands to handle cli execution

---
+++ cmd/root.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "weasel",
+	Short: "Cli to discover wallet with activity and from randomly private key",
+	Long: `Allow search ethereum accounts from a randomly private key.
+Each account with some activity is stored to make subsequent actions.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func SetVersion(version string) {
+	rootCmd.Flags().BoolP("version", "v", false, "Number of version of weasel")
+	rootCmd.Version = version
+}
+
+func init() {
+
+	rootCmd.Version = "unknown"
+
+	rootCmd.RunE = func(cmd *cobra.Command, args []string) error {
+		if cmd.Flag("version").Value.String() == "true" {
+			cmd.Printf(cmd.Version)
+			return nil
+		}
+
+		cmd.HelpFunc()(cmd, args)
+		return nil
+	}
+}
--- main.go
+++ main.go
@@ -3,14 +3,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	_ "embed"
 
-	"github.com/bgadrian/go-mnemonic/bip39"
-	"github.com/mauroalderete/weasel/client"
-	"github.com/mauroalderete/weasel/genwallet"
+	"github.com/mauroalderete/weasel/cmd"
 )
 
 //go:generate build/get_version.sh
@@ -18,51 +13,12 @@ import (
 var version string
 
 func main() {
-
-	const server = "https://cloudflare-eth.com"
-
-	Look(server)
-
-	m, err := bip39.NewMnemonicRandom(128, "")
-	if err != nil {
-		log.Fatal("Whoops something went wrong to make mnemonic generator", err)
-	}
-
-	password, err := m.GetSentence()
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println(password)
-
-	fmt.Printf("Bye\n")
+	cmd.Execute()
 }
 
-func Look(server string) {
-	c := &client.Client{}
-	err := c.Connect(server)
-	if err != nil {
-		log.Fatalf("failed to connect to ethclient: %v", err)
+func init() {
+	if len(version) == 0 {
+		version = "beta"
 	}
-	defer c.Close()
-
-	w, err := genwallet.RandomWallet()
-	if err != nil {
-		log.Fatal("failed instance a new random wallet", err)
-	}
-
-	err = w.Bind(c.Client())
-	if err != nil {
-		log.Fatalf("failed to bind client to wallet: %v", err)
-	}
-
-	err = w.Update()
-	if err != nil {
-		log.Fatalf("failed to update wallet info: %v", err)
-	}
-
-	fmt.Printf("Address: %s\n", w.Address().Hex())
-	fmt.Printf("Balance: %sWEI\n", w.Balance().Wei().String())
-	fmt.Printf("Balance: %sETH\n", w.Balance().Eth().String())
-	fmt.Printf("Pending balance: %sWEI\n", w.PendingBalance().Wei().String())
-	fmt.Printf("Pending balance: %sETH\n", w.PendingBalance().Eth().String())
+	cmd.SetVersion(version)
 }
