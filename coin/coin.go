04aea0ef3a1f123bd819f19beb8da289a4902c05
Mauro Alderete <alderete.mauro@gmail.com>
2022-10-07 19:26:17 -0300

    feat: marshable coin struct

--- coin/coin.go
+++ coin/coin.go
@@ -6,6 +6,21 @@ import (
 	"github.com/mauroalderete/weasel/convert/weieth"
 )
 
+type CoinMarshable struct {
+	Wei int64   `json:"wei"`
+	Eth float64 `json:"eth"`
+}
+
+func (cm *CoinMarshable) Coin() Coin {
+
+	c := Coin{}
+
+	wei := big.NewInt(cm.Wei)
+	c.SetWei(*wei)
+
+	return c
+}
+
 type Coin struct {
 	wei big.Int
 	eth big.Float
@@ -43,4 +58,16 @@ func (c *Coin) SetEth(eth big.Float) error {
 	c.eth = eth
 
 	return nil
+}
+
+func (c *Coin) Marshable() CoinMarshable {
+	wei := c.wei.Int64()
+	eth, _ := c.eth.Float64()
+
+	cm := CoinMarshable{
+		Wei: wei,
+		Eth: eth,
+	}
+
+	return cm
 }
